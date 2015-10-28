fb37c4997046ac9801d2e0fdcfdcc992b09d996f
Steven Karas <steven.karas@gmail.com>
2015-10-28 12:19:41 +0200

    pull port from serverspec in ssh setup

--- .ssh/setup_ssh_server.bash
+++ .ssh/setup_ssh_server.bash
@@ -6,10 +6,15 @@ function setup_ssh_server() {
         username="$(whoami)"
     fi
     local server="${1##*@}"
-    local port="${2:-22}"
-    echo "Setting up $username @ $server with port $port"
+    local port="${1##*:}"
+    if [[ "$port" == "$server" ]]; then
+        port="${2:-22}"
+    else
+        server="${server%:*}"
+    fi
+    echo "Setting up $username@$server:$port"
 
-    tee -a $HOME/.ssh/config <<SSH_CONFIG
+    tee -a $HOME/.ssh/config <<SSH_CONFIG >/dev/null
 
 Host $server
     HostName $server
