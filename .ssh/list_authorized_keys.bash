5b301008be4df731874e46170c658a5c8a19e925
Steven Karas <steven.karas@gmail.com>
2015-10-28 12:22:14 +0200

    list authorized keys

---
+++ .ssh/list_authorized_keys.bash
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+function list_authorized_keys() {
+    local authorized_keys="${1:-$HOME/.ssh/authorized_keys}"
+    if [[ ! -f "$authorized_keys" ]]; then
+        echo "USAGE: list_authorized_keys [FILE]"
+        return 1
+    fi
+    echo "listing keys from $authorized_keys:"
+
+    local -a keys
+    mapfile keys < $authorized_keys
+    local comment=""
+    local fingerprint=""
+    local key=""
+    for i in ${!keys[@]}; do
+        key="${keys[$i]}"
+        if [[ -n "$key" && ${key###} = "$key" ]]; then
+            ssh-keygen -l -f /dev/stdin <<<"$key"
+        fi
+    done
+}
+
+list_authorized_keys $@
