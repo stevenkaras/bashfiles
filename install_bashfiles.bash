af5e101480890a31e0706a4a6b381f6172a70532
Steven Karas <steven.karas@gmail.com>
2016-01-18 15:34:53 +0200

    install script

---
+++ install_bashfiles.bash
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# TODO: automatically gitclone my bashfiles if this script is being piped into bash
+
+function _platform() {
+	local kernel="$(uname -s)"
+	case "$kernel" in
+		Linux)
+			echo "ubuntu"
+			;;
+		Darwin)
+			echo "macos"
+			;;
+	esac
+}
+
+function do_install() {
+	# determine the folder this script is in
+	local ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+	for bashfile in $ROOTDIR/.bash*; do
+		ln -s "$bashfile" "~/$(basename "$bashfile")"
+	done
+	local platform=$(_platform)
+	# inject the bashfiles
+	cat <<-BASH > /.bashrc
+		if [ -f "/home/steven/.bashrc_$platform" ]; then
+		    . "/home/steven/.bashrc_$platform"
+		fi
+
+		if [ -f "/home/steven/.bashlib" ]; then
+		    . "/home/steven/.bashlib"
+		fi
+	BASH
+
+	# Setup binary links
+	mkdir -p ~/bin
+	for binary in $ROOTDIR/bin/*; do
+		ln -s "$binary" "~/bin/$(basename "$binary")"
+	done
+	for ssh_binary in $ROOTDIR/.ssh/*.bash; do
+		ln -s "$ssh_binary" "~/bin/$(basename "${ssh_binary%%.bash}")"
+	done
+}
+
+do_install "$@"
