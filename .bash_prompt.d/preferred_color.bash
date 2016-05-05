bfcd46dece91bc274ebe9ab01fc7f33335ba132b
Steven Karas <steven.karas@gmail.com>
2016-05-05 15:15:16 +0300

    last fix to bash prompt colors

--- .bash_prompt.d/preferred_color.bash
+++ .bash_prompt.d/preferred_color.bash
@@ -1,5 +1,5 @@
 
-if [[ -n "$PREFERRED_COLOR" ]]; then
+if [[ -z "$PREFERRED_COLOR" ]]; then
 	if type -t python >/dev/null 2>&1; then
 		# determine the color based on the user and hostname
 		export PREFERRED_COLOR="$(python -c 'import binascii,getpass,socket;id="%s@%s" % (getpass.getuser(), socket.gethostname(),);hsh=binascii.crc32(id);idx=(hsh % (38-31)) + 31;print(idx)')"
