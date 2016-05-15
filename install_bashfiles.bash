8391e0ee95fb1dd45ccb080ff826a1ad35f62685
Steven Karas <steven.karas@gmail.com>
2016-05-15 17:50:02 +0300

    make install bashfiles idempotent

--- install_bashfiles.bash
+++ install_bashfiles.bash
@@ -19,32 +19,34 @@ function do_install() {
 	local ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 	for bashfile in "$ROOTDIR"/.bash*; do
-		ln -s -T "$bashfile" "$HOME/$(basename "$bashfile")"
+		ln -s -T "$bashfile" "$HOME/$(basename "$bashfile")" 2>/dev/null
 	done
 	local platform=$(_platform)
 	# inject the bashfiles
-	cat <<-BASH >> $HOME/.bashrc
-		if [ -f "$HOME/.bashrc_$platform" ]; then
-		    . "$HOME/.bashrc_$platform"
-		fi
+	if egrep ~/.bashrc -e "(\.|source)\s+('|\")?($HOME|\$HOME)/.bashlib" >/dev/null; then
+		cat <<-BASH >> $HOME/.bashrc
+			if [ -f "\$HOME/.bashrc_$platform" ]; then
+			    . "\$HOME/.bashrc_$platform"
+			fi
 
-		if [ -f "$HOME/.bashlib" ]; then
-		    . "$HOME/.bashlib"
-		fi
-	BASH
+			if [ -f "\$HOME/.bashlib" ]; then
+			    . "\$HOME/.bashlib"
+			fi
+		BASH
+	fi
 
 	# Setup binary links
 	mkdir -p "$HOME/bin"
 	for binary in "$ROOTDIR"/bin/*; do
-		ln -s -T "$binary" "$HOME/bin/$(basename "$binary")"
+		ln -s -T "$binary" "$HOME/bin/$(basename "$binary")" 2>/dev/null
 	done
 	for ssh_binary in "$ROOTDIR"/.ssh/*.bash; do
-		ln -s -T "$ssh_binary" "$HOME/bin/$(basename "${ssh_binary%%.bash}")"
+		ln -s -T "$ssh_binary" "$HOME/bin/$(basename "${ssh_binary%%.bash}")" 2>/dev/null
 	done
 
 	# other files to symlink
 	for otherfile in .tmux.conf .gitignore_global .vimrc .vim .irbrc .psqlrc .lessfilter; do
-		ln -s -T "$ROOTDIR/$otherfile" "$HOME/$otherfile"
+		ln -s -T "$ROOTDIR/$otherfile" "$HOME/$otherfile" 2>/dev/null
 	done
 }
 
