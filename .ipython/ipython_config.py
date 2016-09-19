b875d11a30bc40d276800b23d1f7ab8013fa8ab7
Steven Karas <steven.karas@gmail.com>
2016-09-19 13:11:17 +0300

    add support for older ipython versions to ipython loader

--- .ipython/ipython_config.py
+++ .ipython/ipython_config.py
@@ -1,10 +1,20 @@
+try:
+    c
+except NameError:
+    c = get_config()
 
+c.InteractiveShellApp.setdefault('exec_lines', [])
 c.InteractiveShellApp.exec_lines.extend([
     'import sys, time, dis, os',
 ])
 
 import IPython
+if IPython.version_info > (4,0,0):
+    profile_dir = IPython.paths.locate_profile()
+else: # should apply to at least IPython 0.13.1-1.2.1
+    profile_dir = IPython.utils.path.locate_profile()
 
+c.InteractiveShellApp.setdefault('exec_files', [])
 c.InteractiveShellApp.exec_files.extend([
-    IPython.paths.locate_profile() + '/ipython_helpers.py'
+    profile_dir + '/ipython_helpers.py'
 ])
