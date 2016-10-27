d87b41d0945a37e6474b714662ba0b154a99fe46
Steven Karas <steven.karas@gmail.com>
2016-10-27 17:52:32 +0300

    simplify bash colors
    removed vestigial code that was unused....to be honest, this entire file is...

--- .bash_lib.d/colors.bash
+++ .bash_lib.d/colors.bash
@@ -2,7 +2,7 @@
 
 # set a fancy prompt (non-color, unless we know we "want" color)
 case "$TERM" in
-    xterm-color) color_prompt=yes;;
+	*-color) color_prompt=yes;;
 esac
 
 if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
@@ -13,26 +13,3 @@ if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
 else
     color_prompt=
 fi
-
-setup_bash_colors=
-if [[ -n $setup_bash_colors ]]; then
-    CSI="\033["
-    COLOR_BLACK="${CSI}30m"
-    COLOR_RED="${CSI}31m"
-    COLOR_GREEN="${CSI}32m"
-    COLOR_YELLOW="${CSI}33m"
-    COLOR_BLUE="${CSI}34m"
-    COLOR_PURPLE="${CSI}35m"
-    COLOR_CYAN="${CSI}36m"
-    COLOR_WHITE="${CSI}37m"
-    COLOR_GREY="${CSI}90m"
-    COLOR_LIGHT_RED="${CSI}91m"
-    COLOR_LIGHT_GREEN="${CSI}92m"
-    COLOR_LIGHT_YELLOW="${CSI}93m"
-    COLOR_LIGHT_BLUE="${CSI}94m"
-    COLOR_LIGHT_PURPLE="${CSI}95m"
-    COLOR_LIGHT_CYAN="${CSI}96m"
-    COLOR_LIGHT_GREY="${CSI}97m"
-    COLOR_RESET="${CSI}39m"
-fi
-unset setup_bash_colors
