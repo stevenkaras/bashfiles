1e6a354d77e156eca1cdb1c4ccb228624c082e4a
Steven Karas <steven.karas@gmail.com>
2017-09-15 13:46:55 +0300

    expose previous directory to chdir hooks

--- .bash_lib.d/chdir_hook.bash
+++ .bash_lib.d/chdir_hook.bash
@@ -1,13 +1,16 @@
+#!/usr/bin/env bash
+
 # Change directory hook
 #
 # When the directory is changed, this function will run the CHDIR_COMMAND environment variable
 # You should assume that whatever is already in this command is fully delimited, including semicolons
 function on_chdir() {
     if [ "$PWD" != "$ONCHDIR_OLDPWD" ]; then
-        ONCHDIR_OLDPWD="$PWD"
         eval "$CHDIR_COMMAND"
+        ONCHDIR_OLDPWD="$PWD"
     fi
 }
+
 if [[ ! "$PROMPT_COMMAND" == *"on_chdir"* ]]; then
 	PROMPT_COMMAND="on_chdir${PROMPT_COMMAND:+;$PROMPT_COMMAND}"
 fi
